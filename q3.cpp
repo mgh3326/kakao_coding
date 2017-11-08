883c776bb7e4d1695035abe855147cd4093fefe8
mgh3326 <mgh3326@naver.com>
2017-11-08 08:16:17 +0000

    first

---
+++ q3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+vector<string> v;
+void function(istream& ins)
+{
+	int i=0;
+	while(ins && ins.peek !=\n)
+	{
+		if(ins==',')
+		{
+			cin.ignore();
+		}
+		else{
+
+			v[i].pushback(
+		
+		}
+	}
+}
+int main(void){
+	//cout<<"test"<<endl;
+	int cashsize=0;
+	cin>>cashsize;
+	function(cin);
+	return 0;
+}
